8d2b2ad51fb92ff63c41db01ca5d2bf5ae272486
Sashen Jayathilaka <99184393+SashenJayathilaka@users.noreply.github.com>
2023-01-03 22:31:52 +0530

    🎯 Dart Fundamentals

---
+++ Dart Programming/COMMENTS/main.dart
@@ -0,0 +1,12 @@
+/* void main() {
+
+  // This is first line of code
+  
+  print("Hello World");
+  
+  print("Dart Hello World"); // this is another comments
+  
+  print(4 - 1);
+  
+  print(true);
+} */
---
+++ Dart Programming/Dart Strings, Literals and String Interpolation/main.dart
@@ -0,0 +1,30 @@
+void main() {
+  // Literals
+  var isCool = true;
+  int x = 2;
+  true;
+  2;
+  "John";
+  4.5;
+
+  String s1 = "Single";
+  String s2 = "Double";
+  String s3 = 'It\'s easy';
+  String s4 = "It\'s easy";
+
+  String s5 = 'Dart Strings, Literals and' 'String Interpolation';
+
+  // String Interpolation
+
+  String name = "Sashen";
+  String message = "My name is " + name;
+  String sMessage = "My name is $name";
+
+  print(s3);
+  print(s4);
+  print(s5);
+  print(message);
+  print(sMessage);
+  print("The number of the charter in string $name is " + name.length.toString());
+  print("The number of the charter in string $name is $name.length");
+}
