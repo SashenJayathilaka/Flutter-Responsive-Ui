9849d74a6e1448319ee6cc2cc04a927e5e785216
Sashen Jayathilaka <99184393+SashenJayathilaka@users.noreply.github.com>
2023-01-03 22:45:19 +0530

    🎯 Dart Fundamentals

---
+++ Dart Programming/Defining Constants/main.dart
@@ -0,0 +1,15 @@
+void main() {
+// final keyword
+  final cityName = "Colombo";
+  // cityName = "Gall";
+
+  final String countryName = "SriLanka";
+
+  const PI = 3.14;
+  const double gravity = 9.8;
+}
+
+class Circle {
+  final color = "Red";
+  static const PI = 3.14;
+}
